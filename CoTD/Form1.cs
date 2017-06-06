8a754cb342960d462e07bcfc6c05bc5d1173dc85
CUB3D <callum.thomson@discoveryschool.org.uk>
2017-06-06 15:48:18 +0100

    Refactored config loading

--- CoTD/Form1.cs
+++ CoTD/Form1.cs
@@ -13,31 +13,37 @@ namespace CoTD
 {
     public partial class Form1 : Form
     {
-        public static List<string> alpha = new List<string>();
+        public static Dictionary<string, string> config = new Dictionary<string, string>();
+
         public Form1()
         {
             InitializeComponent();
+            loadConfig();
+        }
+
+        public void loadConfig()
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader("test.txt");
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] data = line.Split('=');
+                config.Add(data[0], data[1]);
+            }
+
+            file.Close();
+        }
+
+        public void loadPage(string url)
+        {
+            webBrowser1.Url = new Uri(url);
+            webBrowser1.Refresh();
         }
 
         public void init()
         {
-            int counter = 0;
-            string line;
-
-            // Read the file and display it line by line.
-            System.IO.StreamReader file =
-               new System.IO.StreamReader("test.txt");
-            while ((line = file.ReadLine()) != null)
-            {
-                alpha.Add(line);
-                counter++;
-            }
-
-            file.Close();
-
             // Suspend the screen.
-            Console.ReadLine();
-            webBrowser1.Url = new Uri(alpha[0]);
+            webBrowser1.Url = new Uri(config["CodeURL"]);
             webBrowser1.Refresh();
             string s = webBrowser1.DocumentText;
        
@@ -48,7 +54,7 @@ namespace CoTD
                 string sss = new Regex(">[a-z]{8}<").Match(s).Value.Substring(1, 8);
 
                 MessageBox.Show(sss);
-                webBrowser1.Url = new Uri(alpha[1]);
+                webBrowser1.Url = new Uri(config["loginURL"]);
                 HtmlElementCollection inputObjects = webBrowser1.Document.GetElementsByTagName("user_password");
                 inputObjects[0].SetAttribute("",sss);
             }
