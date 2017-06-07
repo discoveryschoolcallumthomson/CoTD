3e0f420f999eefdfd4b05c355440d4534b64a3d2
CUB3D <CUB3D@CALLUMS8PROVM>
2017-06-07 15:49:30 +0100

    Removed the hacky code for detecting the stage in the login procedure

--- CoTD/Form1.cs
+++ CoTD/Form1.cs
@@ -17,15 +17,14 @@ namespace CoTD
         public static Dictionary<string, string> config = new Dictionary<string, string>();
         public static Regex COTDMatcher = new Regex(">[a-z]{8}<");
 
+        public static WebBrowserDocumentCompletedEventHandler oldEventHandle = null;
+
         public static string COTD = "";
 
         public Form1()
         {
             InitializeComponent();
             loadConfig();
-            
-            webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(onPageLoaded);
-            webBrowser1.AllowNavigation = true;
         }
 
         public void loadConfig()
@@ -45,7 +44,7 @@ namespace CoTD
         {
             //TODO
             webBrowser1.Navigate(url);
-            webBrowser1.Url = new Uri(url);
+           // webBrowser1.Url = new Uri(url);
             webBrowser1.Refresh();
         }
 
@@ -54,19 +53,26 @@ namespace CoTD
             loadPage(config[keyName]);
         }
 
+        public void setDocumentLoadHandler(Action<object, WebBrowserDocumentCompletedEventArgs> func)
+        {
+            //Remove the old event listener
+            if(oldEventHandle != null)
+            { 
+                webBrowser1.DocumentCompleted -= oldEventHandle;
+            }
+            oldEventHandle = new WebBrowserDocumentCompletedEventHandler(func);
+            webBrowser1.DocumentCompleted += oldEventHandle;
+        }
+
         public void getCoTD()
         {
+            setDocumentLoadHandler(onCoTDPageLoad);
             loadPageFromConfig(DEBUG ? "debugCodeURL" : "codeURL"); 
         }
 
-        public void init()
+        public void onCoTDPageLoad(object sender, WebBrowserDocumentCompletedEventArgs asdf)
         {
-            getCoTD();
-        }
-
-        public void onPageLoaded(object sender, WebBrowserDocumentCompletedEventArgs asdf)
-        {
-            WebBrowser browser = sender as WebBrowser;   
+            WebBrowser browser = sender as WebBrowser;
 
             try
             {
@@ -75,31 +81,47 @@ namespace CoTD
                 if (COTD != null && COTD.Length > 0)
                 {
                     label1.Text = "COTD: " + COTD;
+                    setDocumentLoadHandler(onLoginPageLoaded);
                     loadPageFromConfig(DEBUG ? "debugLoginURL" : "loginURL");
                 }
 
 
             } catch (Exception e) {
-
-                try
-                {
-                    HtmlElementCollection inputObjects = webBrowser1.Document.GetElementsByTagName("input");
-
-                    foreach(HtmlElement obj in inputObjects)
-                    {
-                        if (obj.Name == "user_password")
-                        {
-                            obj.SetAttribute("value", COTD);
-                        }
-                    }
-
-                    HtmlElement submit = webBrowser1.Document.GetElementById("fsloginButton");
-                    submit.InvokeMember("Click");
-                } catch (Exception ee) {
-                    //MessageBox.Show("Unable to get the CoTD, Are you connected to DSCoTD?");
-                    MessageBox.Show("You should now be logged in");
-                }
+                MessageBox.Show("Unable to get the CoTD, Are you connected to DSCoTD?");
             }
+        }
+
+        public void onLoginPageLoaded(object sender, WebBrowserDocumentCompletedEventArgs asdf)
+        {
+            try
+            {
+                HtmlElementCollection inputObjects = webBrowser1.Document.GetElementsByTagName("input");
+
+                foreach (HtmlElement obj in inputObjects)
+                {
+                    if (obj.Name == "user_password")
+                    {
+                        obj.SetAttribute("value", COTD);
+                    }
+                }
+
+                setDocumentLoadHandler(onLoginComplete);
+
+                HtmlElement submit = webBrowser1.Document.GetElementById("fsloginButton");
+                submit.InvokeMember("Click");
+            } catch (Exception ee) {
+                MessageBox.Show("Error clicking button");
+            }
+        }
+
+        public void onLoginComplete(object sender, WebBrowserDocumentCompletedEventArgs asdf)
+        {
+            MessageBox.Show("You should now be logged in");
+        }
+
+        public void init()
+        {
+            getCoTD();
         }
 
         private void button1_Click(object sender, EventArgs e)
